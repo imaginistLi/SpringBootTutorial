7184fd38a4b555ff18be7e49546ad251cdbaa443
imaginistLi <imaginist@sjtu.edu.cn>
2021-08-26 23:15:40 +0800

    spring-boot-01-helloworld

---
+++ spring-boot-01-helloworld/src/main/java/imaginist/HelloWorldMainApplication.java
@@ -0,0 +1,14 @@
+package imaginist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+
+    public static void main(String[] args) {
+
+        // Spring 应用启动
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+}
