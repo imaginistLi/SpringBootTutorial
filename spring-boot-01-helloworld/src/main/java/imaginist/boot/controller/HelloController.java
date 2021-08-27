33c3ed21766870c8ba3fb030f8744d3cc4c5c835
imaginistLi <imaginist@sjtu.edu.cn>
2021-08-27 20:27:20 +0800

    SpringBoot2

--- spring-boot-01-helloworld/src/main/java/imaginist/boot/controller/HelloController.java
+++ spring-boot-01-helloworld/src/main/java/imaginist/boot/controller/HelloController.java
@@ -1,15 +1,15 @@
-package imaginist.controller;
+package imaginist.boot.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @ResponseBody
     @RequestMapping("/hello")
     public String hello(){
-        return "Hello, World!";
+        return "Hello, Spring Boot 2!";
     }
 }
